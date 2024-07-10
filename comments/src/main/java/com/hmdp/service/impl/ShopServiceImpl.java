4cf320d749d3726e75c9c8be2c4dc6f6227eaa1a
Chad <2420811595@qq.com>
2024-07-10 11:12:15 +0800

    feature:店铺缓存和数据库的一致性解决

--- comments/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ comments/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -13,8 +13,10 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -47,7 +49,20 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             return Result.fail("该商户不存在");
         }
         //6.将查询到的数据存入redis
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
+    }
+
+    @Override
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if(id == null){
+            return  Result.fail("商户ID不能为空");
+        }
+        //1.修改数据库
+        updateById(shop);
+        //2.删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
+        return Result.ok();
     }
 }
