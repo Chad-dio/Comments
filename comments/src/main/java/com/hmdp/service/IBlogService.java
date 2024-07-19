ee69df278dc57be864d9321c4524476b4baaca09
Chad <2420811595@qq.com>
2024-07-19 13:50:14 +0800

    featur:实现点赞收藏功能

--- comments/src/main/java/com/hmdp/service/IBlogService.java
+++ comments/src/main/java/com/hmdp/service/IBlogService.java
@@ -1,9 +1,11 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 
 public interface IBlogService extends IService<Blog> {
 
+    Result likeBlog(Long id);
 }
--- comments/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ comments/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
 
 /**
  * <p>
@@ -16,5 +24,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result likeBlog(Long id) {
+        //1.获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        //2.查看用户是否对该博客点过赞
+        String key = BLOG_LIKED_KEY + id;
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        if(score == null){
+            //3.如果未点过赞
+            //3.1 更新点赞数
+            boolean update = update().setSql("liked = liked + 1").eq("id", id).update();
+            if(BooleanUtil.isTrue(update)){
+                //3.2 将该用户添加到点赞集合里
+                stringRedisTemplate.opsForZSet().add(key, userId.toString(), System.currentTimeMillis());
+            }
+        }else{
+            boolean update = update().setSql("liked = liked - 1").eq("id", id).update();
+            if(BooleanUtil.isTrue(update)){
+                //3.2 将该用户从点赞集合里删除
+                stringRedisTemplate.opsForZSet().remove(key, userId.toString());
+            }
+        }
+        return Result.ok();
+    }
 }
